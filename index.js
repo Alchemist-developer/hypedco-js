6de98e03429366780c9ea24ba56794d1a79eddff
Simon | @ <88943961+Alchemist-developer@users.noreply.github.com>
2022-03-18 20:35:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+// 1
 const companhia = 'Hyped CO';
+console.log(`*----------------*----------------*`);
 console.log(`\nWelcome to ${company}\n`);
+console.log(`*----------------*----------------*`);
 
+// 2
 var roupas = [];
 
+// 3
 roupas = [
     {
         id: Math.random().toString().substr(2, 6),
@@ -16,8 +21,8 @@ roupas = [
     }
 ];
 
+// 4
 const addRoupa = (titulo, tamanho, tecido, video, infantil) => {
-
     lista.push({
         id: Math.random().toString().substr(2, 6),
         titulo,
@@ -26,29 +31,28 @@ const addRoupa = (titulo, tamanho, tecido, video, infantil) => {
         video,
         infantil
     });
-
     console.log('Sua lista de roupas:\n');
     console.log(roupas);
     console.log(`\nPeça "${titulo}" adicionada com sucesso!\n`);
-    console.log('---------------------------------------');
+    console.log(`*----------------*----------------*`);
 }
 
 addRoupa('nice shirt', 'g/gg', ['poliester', 'algodão'], 'https://www.google.com', false);
 addRoupa('nice jeans', 'p/m', ['poliester', 'jeans'], 'https://www.google.com', true);
 
+// 5
 const mostrarRoupa = () => {
     roupas.forEach(item => {
         console.log(`Item: "${item.title}"`);
-
         console.log(`Tamanho:\n> ${item.tamanho}`);
-
         console.log(`Infantil: ${item.infantil}\n`);
-        console.log('---------------------------------------');
+        console.log(`*----------------*----------------*`);
     })
 };
 
 mostrarRoupa();
 
+// 6
 const deletaRoupa = id => {
     const novaRoupa = roupas.filter(item => item.id != id);
     const roupa = roupas.filter(item => !novaRoupa.includes(item));
@@ -61,10 +65,12 @@ const deletaRoupa = id => {
     console.log(`Peça "${id} - ${roupa[0].title}"deletada!\n`);
     console.log('\nSua lista de Roupas\n');
     console.log(roupas);
+    console.log(`*----------------*----------------*`);
 }
 
 // deletaRoupa();
 
+// 7
 const buscaRoupas = campo => {
     const novaRoupa = roupas.filter(item => item.title.toLowerCase().includes(campo.toLowerCase()));
 
@@ -73,13 +79,15 @@ const buscaRoupas = campo => {
     }
 
     let resultado = novaRoupa.length;
-    console.log(`\nRoupas containing "${campo}": ${results}\n`);
+    console.log(`\nConteúdo de Roupas "${campo}": ${results}\n`);
     console.log(novaRoupa);
+    console.log(`*----------------*----------------*`);
 }
 
 buscaRoupas('hyped');
 buscaRoupas('autentic');
 
+// 8
 const atualizaRoupas = (id, roupa) => {
     let atualizado = false;
 
@@ -102,15 +110,17 @@ const atualizaRoupas = (id, roupa) => {
             }
             atualizado = true;
             console.log(`\nRoupa "${id} - ${item.title}" atualizada!\n`);
+            console.log(`*----------------*----------------*`);
         }
     });
 
     if (!atualizado) {
         return console.log(`\nRoupa "${id}" não encontrada\n`);
     }
-
     console.log('Sua lista de Roupas:\n');
+    console.log(`*---------------------------------*`);
     console.log(roupas);
+    console.log(`*---------------------------------*`);
 }
 
 // atualizaRoupas(); 
